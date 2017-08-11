a2d2b7e91d7119d9c2dbecddcba1df01beea31d3
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-10 19:31:28 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.send('Article one requested and will be send here');
+   res.sendFile(path.join(__dirname, 'ui', 'Article 1'));
 });    
  
 app.get('/article-two', function (req, res){
