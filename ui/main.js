b10055a2b048368cdbe225207cffbe115e91b14b
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-16 04:23:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'I am vaaman who are you';
 
 var img = document.getElementById('madi');
-var marginLeft = 0;
+var marginLeft = 10;
 function moveRight () {
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function (){
-    var interval = setInterval (moveRight, 50);
+    var interval = setInterval (moveRight, 1000);
     
 };
