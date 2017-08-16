9a0364d0572e8882d9adef6364f9cc95a287b046
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-16 04:11:14 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'I am vaaman who are you';
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function (){
     img.style.marginLeft = '100px';
 };
