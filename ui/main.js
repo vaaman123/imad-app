a74b19e02aa7d4a9bbe6a842c8bbb435975b5284
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-17 19:04:48 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,15 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function (){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+    var request = new XMLHttpRequest ();
+    request.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var counter = responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://vaaman234.imad.hasura-app.io/counter', true);
+    request.send (null);
 };
