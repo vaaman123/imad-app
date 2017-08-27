41529f2b497136cd5aa6537b6b32d0b847e4cc63
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-26 20:46:00 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,11 +107,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input){
-    var hashed = crypto.pbkdf2Sync()
+function hash (input, salt){
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
 }
 app.get('/hash/:input', function (req, res) {
-    var hashedString = hash (req.params.input);
+    var hashedString = hash (req.params.input, 'this-is-some-silly-file');
     res.send(hashedString);
 });
 var counter = 0;
