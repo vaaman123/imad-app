5f482a743774ee223781b78a958118313749037b
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-26 20:10:00 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+
+var crypto = require ('crypto');
 var config = {
     user: 'vaman234',
     database: 'vaaman234',
@@ -106,26 +107,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-var pool = new Pool(config);
-app.get ('/test-db', function (req, res){
-    pool.query('SELECT * FROM test', function(err, result){
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON.stringfy(result.rows));
-       }
-    });
+function hash (input){
+    var hashed = crypto.pbkdf2Sync()
+}
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash (req.params.input);
+    res.send(hashedString);
 });
-
 var counter = 0;
-app.get('/counter', function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-    
-    
-
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));  
