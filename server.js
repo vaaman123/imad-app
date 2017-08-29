177c5d8355271dc664438ddef7e90648a0fffdc3
vaaman123 <30763681+vaaman123@users.noreply.github.com>
2017-08-28 19:11:58 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/hash/:input', function (req, res) {
 app.post('/create-user', function (req, res){
     var username = req.body.username;
     var password = req.body.password;
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.getrandomBytes(128).toString('hex');
     var dbString = hash(password, salt); 
     pool.query ('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
        if (err) {
